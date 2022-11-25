1be6e0f93b9133596e4ac04018eeaf2b51a126bc
Federico Ruiz <82234080+FedericoRuiz3@users.noreply.github.com>
2022-11-25 20:38:00 -0300

    Delete Azanza Facundo-Federico Ruiz 3º BC directory

--- Azanza Facundo-Federico Ruiz 3º BC/PHP/Estudiante/buscar.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    include_once "../connect.php";
-    require '../conexion.php';
-    $Nota = $_POST['nota'];
-	$stmt = $pdo->query("SELECT * FROM promedio WHERE nota = '$Nota' ");
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($rows);
-?>
--- Azanza Facundo-Federico Ruiz 3º BC/PHP/Estudiante/mostrar.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    require '../conexion.php';
-	$sql="SELECT * from estudiante";
-	$result=mysqli_query($conexion,$sql);
-	while($mostrar=mysqli_fetch_array($result)){
-	}
-?>
